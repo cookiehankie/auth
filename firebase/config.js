725d7c9c0c9eef81278c32f82a6f110001438b66
Yucheng <hankgycc@gmail.com>
2024-02-09 16:16:44 -0500

    restructure

--- firebase/config.js
+++ firebase/config.js
@@ -10,13 +10,14 @@ import { getAuth } from "firebase/auth";
 const firebaseConfig = {
   // Your web app's Firebase configuration here
   // See https://firebase.google.com/docs/web/setup#add-sdks-initialize
-  apiKey: "AIzaSyC1fJin7rP6jMtzdemmSgGrkoOX67cWTGA",
-  authDomain: "ezout-backend.firebaseapp.com",
-  projectId: "ezout-backend",
-  storageBucket: "ezout-backend.appspot.com",
-  messagingSenderId: "520617196632",
-  appId: "1:520617196632:web:042809a5b11cee2c5c5e2d",
-  measurementId: "G-45G1LER5SS"
+  apiKey: 'API_KEY',
+  authDomain: 'PROJECT_ID.firebaseapp.com',
+  databaseURL: 'https://PROJECT_ID.firebaseio.com',
+  projectId: 'PROJECT_ID',
+  storageBucket: 'PROJECT_ID.appspot.com',
+  messagingSenderId: 'SENDER_ID',
+  appId: 'APP_ID',
+  measurementId: 'G-MEASUREMENT_ID',
 };
 
 // Initialize Firebase
